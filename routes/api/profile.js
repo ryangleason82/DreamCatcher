b9821c588ebd1bf1d39abef84a44ab8aac8a2fac
Ryan Gleason <rjg5404@psu.edu>
2019-03-14 06:45:42 -0400

    Profile backend complete

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -24,6 +24,7 @@ router.get(
 	(req, res) => {
 		const errors = {};
 		Profile.findOne({ user: req.user.id })
+			.populate("user", ["name", "avatar"])
 			.then(profile => {
 				if (!profile) {
 					errors.noprofile = "There is no profile for this user";
@@ -35,10 +36,64 @@ router.get(
 	}
 );
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get("/all", (req, res) => {
+	Profile.find()
+		.populate("user", ["name", "avatar"])
+		.then(profiles => {
+			if (!profiles) {
+				errors.noprofile = "There are no profiles";
+				return res.status(404).json();
+			}
+			res.json(profiles);
+		})
+		.catch(err => res.status(404).json({ profile: "There are no profiles" }));
+});
+
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get("/handle/:handle", (req, res) => {
+	const errors = {};
+	Profile.findOne({ handle: req.params.handle })
+		.populate("user", ["name", "avatar"])
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = "There is no profile for this user";
+				res.status(404).json(errors);
+			}
+
+			res.json(profile);
+		})
+		.catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", (req, res) => {
+	const errors = {};
+	Profile.findOne({ user: req.params.user_id })
+		.populate("user", ["name", "avatar"])
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = "There is no profile for this user";
+				res.status(404).json(errors);
+			}
+
+			res.json(profile);
+		})
+		.catch(err =>
+			res.status(404).json({ profile: "There is no profile for this user" })
+		);
+});
+
 // @route   POST api/profile/
 // @desc    Create or edit profile
 // @access  Private
-router.get(
+router.post(
 	"/",
 	passport.authenticate("jwt", { session: false }),
 	(req, res) => {
