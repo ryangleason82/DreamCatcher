79a625d61f3d82ee6ed5b1cb5d74c48a53167de5
Ryan Gleason <rjg5404@psu.edu>
2019-04-05 06:52:40 -0400

    changed env name

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "Dreamcatcher-env") {
 	// Set static folder
 	app.use(express.static("client/build"));
 	app.get("*", (req, res) => {
