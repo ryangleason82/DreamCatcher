a644de37cf7b340de4a57e54a5fabe66dbee6129
Ryan Gleason <rjg5404@psu.edu>
2019-04-05 07:05:52 -0400

    'change'

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 // Server static assets if in production
-if (process.env.NODE_ENV === "Dreamcatcher-env") {
+if (process.env.NODE_ENV === "production") {
 	// Set static folder
 	app.use(express.static("client/build"));
 	app.get("*", (req, res) => {
