e5560596365149668e315a3b5c0a1607663c00d9
Ryan Gleason <rjg5404@psu.edu>
2019-04-06 12:43:08 -0400

    'b'

---
+++ client/build/precache-manifest.d67bcd4f80bc1949f93e5600163697ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4d7ba4588f33a2f361700deb82448ae",
+    "url": "/static/media/showcase.f4d7ba45.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8bb5e48291f7c311e57",
+    "url": "/static/js/main.dbe81370.chunk.js"
+  },
+  {
+    "revision": "449d7fdb382abc29a258",
+    "url": "/static/js/2.c6657532.chunk.js"
+  },
+  {
+    "revision": "d8bb5e48291f7c311e57",
+    "url": "/static/css/main.d9b0f5b2.chunk.css"
+  },
+  {
+    "revision": "449d7fdb382abc29a258",
+    "url": "/static/css/2.8a53cad7.chunk.css"
+  },
+  {
+    "revision": "f74cde36a72f586594a32a846e236880",
+    "url": "/index.html"
+  }
+];
