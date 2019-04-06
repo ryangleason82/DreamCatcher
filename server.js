d4011b2966b42fdd20c7ba1d8d59859416c0e56d
Ryan Gleason <rjg5404@psu.edu>
2019-04-06 12:32:31 -0400

    a

--- server.js
+++ server.js
@@ -38,9 +38,10 @@ console.log("Do we get here");
 // Server static assets if in production
 //if (process.env.NODE_ENV === "Dreamcatcher-env") {
 console.log("how bout here");
+console.log(process.env.NODE_ENV);
 // Set static folder
-app.use(express.static("client/build"));
-app.get("*", (req, res) => {
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 console.log("probably not here");
